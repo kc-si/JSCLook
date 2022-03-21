2884c7e863df82e07e9c8483f840a134ff8afa6a
kc-si <kciupak@o2.pl>
2022-03-21 19:45:28 +0100

    Update tests

--- spec/requests/companies_spec.rb
+++ spec/requests/companies_spec.rb
@@ -25,3 +25,16 @@ RSpec.describe 'Companies', type: :request do
     end
   end
 end
+
+RSpec.describe 'GET /companies/:id', type: :request do
+  it 'displays selected company' do
+    company = Company.create!(name: 'MOSTALPLC', symbol: 'PLMSTPL00018')
+    id = company.id
+
+    get "/companies/#{id}"
+
+    expect(response).to have_http_status(:success)
+    expect(response.body).to include('Company')
+    expect(response.body).to include('MOSTALPLC')
+  end
+end
