46333fdfc193216aedd84b5eee9aac0943b9e213
kc-si <kciupak@o2.pl>
2022-04-02 22:38:55 +0200

    Add legal reservations request test

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'GET /legal_reservations', type: :request do
+  it 'renders legal reservations' do
+    get legal_reservations_path
+
+    expect(response).to have_http_status(:success)
+    expect(response.body).to include('complete and truthful')
+  end
+end
