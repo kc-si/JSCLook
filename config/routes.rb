6053dd6a9947c8d2b0e520e34041904e47515822
kc-si <kciupak@o2.pl>
2022-04-11 16:52:44 +0200

    Fix request sending method-type bug
    App Change every sending method to GET.
    Change link_to helper to button_to solve this problem.
    Upgrade app (add missing code from new generated app)

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,9 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root 'companies#index'
 
-  get '/companies/update_companies_list', to: 'companies#update_companies_list'
-  get '/companies/update_companies_details', to: 'companies#update_companies_details'
+  post '/companies/update_companies_list', to: 'companies#update_companies_list'
+  post '/companies/update_companies_details', to: 'companies#update_companies_details'
+
   get '/legal_reservations', to: 'static_pages#legal_reservations'
 
   resources :companies, only: [:index, :show]
